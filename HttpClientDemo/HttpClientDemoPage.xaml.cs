27c61d7317fe5b4384fd216466ac8ba3a53a884a
smoming <smoming1031@gmail.com>
2017-04-08 01:31:21 +0800

    HttpClient_Example
    HttpClient Example

---
+++ HttpClientDemo/HttpClientDemoPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace HttpClientDemo
+{
+	public partial class HttpClientDemoPage : ContentPage
+	{
+		public HttpClientDemoPage()
+		{
+			InitializeComponent();
+			LoadData();
+		}
+
+		async void LoadData()
+		{
+			var _client = new HttpClient();
+			var jsonvalue = await _client.GetAsync("http://ins-info.ib.gov.tw/opendata/json-05010111.aspx");
+			if (jsonvalue.IsSuccessStatusCode)
+			{
+				listView.ItemsSource =
+					JsonConvert.DeserializeObject<List<InsuranceCompany>>(
+								jsonvalue.Content.ReadAsStringAsync().Result);
+			}
+		}
+	}
+}
---
+++ HttpClientDemo/InsuranceCompany.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace HttpClientDemo
+{
+	public class InsuranceCompany
+	{
+		public string INSURER_Name { get; set; }
+		public string EstablishDate { get; set; }
+		public string ManagerName1 { get; set; }
+		public string ManagerName2 { get; set; }
+		public string Tel { get; set; }
+		public string Fax { get; set; }
+		public string FreeLine { get; set; }
+		public string Address { get; set; }
+		public string WebAddress { get; set; }
+		public string Email { get; set; }
+	}
+}
